fc1f737b5245d12b2e3197393661abe0f7863ff8
Nicolò Dalmazzo <nico.dalmazzo@proton.me>
2026-05-14 10:59:09 +0200

    add libtpms package

---
+++ package/cca-libtpms/libtpms.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# libtpms
+#
+################################################################################
+CCA_LIBTPMS_VERSION = 0.10.0
+CCA_LIBTPMS_SITE = https://github.com/stefanberger/libtpms.git
+
+CCA_LIBTPMS_SITE_METHOD = git
+CCA_LIBTPMS_LICENSE = BSD-4-Clause
+CCA_LIBTPMS_LICENSE_FILES = LICENSE
+
+CCA_LIBTPMS_DEPENDENCIES += host-pkgconf openssl
+
+CCA_LIBTPMS_CONF_OPTS = \
+    --with-openssl \
+	--with-tpm2 \
+    --without-tpm1
+
+$(eval $(autotools-package))
