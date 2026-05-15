4ba61676c38b0e7d66e98e1a6931ae7df695c390
Nicolò Dalmazzo <nico.dalmazzo@proton.me>
2026-05-15 13:32:21 +0200

    minimal version of test and enable strace

--- package/cca-swtpm/swtpm.mk
+++ package/cca-swtpm/swtpm.mk
@@ -5,11 +5,12 @@ CCA_SWTPM_SITE_METHOD = git
 CCA_SWTPM_LICENSE = BSD-4-Clause
 CCA_SWTPM_LICENSE_FILES = LICENSE
 
-CCA_SWTPM_DEPENDENCIES += cca-libtpms ss netstat
+CCA_SWTPM_DEPENDENCIES += cca-libtpms
 
 CCA_SWTPM_AUTORECONF = YES
 
 CCA_SWTPM_CONF_OPTS = \
+	--disable-tests \
     --with-openssl \
     --with-tpm2 \
     --without-selinux \
